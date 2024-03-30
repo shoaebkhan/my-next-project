78d9fdb32a1cc6d4d6492ae4b527dd1267d42e1c
Shoaib Khan <shoaebk@outlook.com>
2024-03-30 12:12:57 +0500

    Added two route segments

---
+++ src/app/name/address/page.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function address() {
+  return <h1>Hello, I am [shoaib khan] and this is my address Next.js page.</h1>
+}
---
+++ src/app/name/page.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function name() {
+  return <h1>Hello, I am [shoaib khan] and this is my name Next.js page.</h1>
+}
